a152afaef4186a75ff43ac6ffc51fcd1196ea4e2
Jack Richard-Bollans <jack.richard-bollans@f-secure.com>
2021-03-31 22:19:58 +0300

    Add functions for generating public and private keys

--- generateKey.go
+++ generateKey.go
@@ -11,10 +11,19 @@ import (
 
 func main() {
 		rand.Seed(time.Now().UnixNano())
-        block := createRandomBlock()
-        fmt.Printf("%x\n", block)
+        // block := createRandomBlock()
+        // fmt.Printf("%x\n", block)
 
-        fmt.Printf("%x", sha256.Sum256(block))
+        // fmt.Printf("%x\n", sha256.Sum256(block))
+
+        privateKey := generatePrivateKey()
+        fmt.Printf("%x\n", privateKey[0])
+
+        publicKey := generatePublicKey(privateKey)
+
+        // fmt.Printf("%x\n", sha256.Sum256(key[0][0]))
+
+        fmt.Printf("%x\n", publicKey[0])
 }
 // func generatePrivateKey() string {
 // 	a := rand.Uint64()
@@ -27,4 +36,26 @@ func createRandomBlock()  []byte {
 	rand.Read(block)
 	return block
 
+}
+
+func generatePrivateKey() [256][2] []byte {
+	var key [256][2] []byte
+	for j := 0; j < 2; j++ {
+		for i := 0; i < 256; i++ {
+			key[i][j] = createRandomBlock()
+		}
+	}
+	return 	key
+}
+
+func generatePublicKey(privateKey [256][2][]byte) [256][2] []byte{
+	var publicKey [256][2] []byte
+	for j := 0; j < 2; j++ {
+		for i := 0; i < 256; i++ {
+			block := sha256.Sum256(privateKey[i][j])
+			// fmt.Printf("%x\n", block)
+			publicKey[i][j] = block[:]
+		}
+	}
+	return publicKey
 }
